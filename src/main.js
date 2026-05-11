543b773922e31da5386adfc791f116ad64570b70
Byatara <gitaris.byan@gmail.com>
2026-05-11 20:50:30 +0700

    Fix:logic code update in main.js

--- src/main.js
+++ src/main.js
@@ -11,21 +11,34 @@ class GameScene extends Phaser.Scene{
     constructor(){
         super("scene-game");
         this.player;
+        this.cursor;
         this.playerSpeed=speedDown+50;
+        this.target;
     }
     preload(){
         this.load.image("bg","/assets/bg.png");
         this.load.image("basket","/assets/basket.png");
+        this.load.image("apple","/assets/apple.png");
     }
     create(){
         this.add.image(0,0,"bg").setOrigin(0,0);
-        this.player=this.physics.add.image(0,sizes.height-100,"basket").setOrigin(0,0);
+        this.player=this.physics.add
+            .image(0,sizes.height - 100,"basket")
+            .setOrigin(0, 0);
         this.player.setImmovable(true);
         this.player.body.allowGravity=false;
+        this.player.setCollideWorldBounds(true);
 
+        this.target=this.physics.add
+            .image(0,0,"apple")
+            .setOrigin(0,0);
         this.cursor=this.input.keyboard.createCursorKeys();
     }
     update(){
+        if (this.target.y >= sizes.height){
+            this.target.setY(0);
+        }
+
         const {left,right}=this.cursor;
         if(left.isDown){
             this.player.setVelocityX(-this.playerSpeed);
